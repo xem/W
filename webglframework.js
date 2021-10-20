acda552d7886cf485b66946b532c45f20bb13590
Staś Małolepszy <stas@piesku.com>
2021-10-20 23:01:02 +0200

    Flip textures when they're created

--- webglframework.js
+++ webglframework.js
@@ -107,7 +107,7 @@ W = {
       let texture = gl.createTexture();
       gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
       gl.bindTexture(gl.TEXTURE_2D, texture);
-      //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
       gl.generateMipmap(gl.TEXTURE_2D);
       return texture;
